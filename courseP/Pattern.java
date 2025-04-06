52209b73d1c6123b1db923ab93dbc4ba1554bd2f
Arpita Kashyap <arpitak1912@gmail.com>
2025-04-06 18:09:04 +0530

    spring courses

--- courseP/Pattern.java
+++ courseP/Pattern.java
@@ -1,44 +1,6 @@
 public class Pattern {
     public static void main(String[] args) {
 
-        /*
-         * below tringle is
-         * 1
-         * 12
-         * 123
-         * 1234
-         * 12345
-         * 123456
-         * 1234567
-         */
-        // for (int i = 1; i <= 7; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print(j);
-        // }
-        // System.out.println();
-        // }
-
-        // square
-        // for (int i = 1; i <= 5; i++) {
-        // for (int j = 1; j <= 5; j++) {
-        // System.out.print("*");
-        // }
-        // System.err.println();
-        // }
-        // tringle
-        /*
-        *****
-        ****
-        ***
-        **
-        *
-        */
-        // for (int i = 1; i <= 5; i++) {
-        // for (int j = 1; j <= 5 - i + 1; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
         // triangle up
         /*
         *
@@ -54,60 +16,163 @@ public class Pattern {
         // System.out.println();
         // }
 
-        // up triangle with increasing count
-        /*
-         * 1
-         * 23
-         * 456
-         * 78910
-         * 1112131415
-         */
-        // int n = 1;
-        // for (int i = 1; i <= 5; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.err.print(n++);
-        // }
-        // System.out.println();
-        // }
+        num_triangle(7);
+        System.out.println();
+        downTriangleStar(5);
+        System.out.println();
+        square(5);
+        System.out.println();
+        downTriangle(5);
+        System.out.println();
+        downTrianglenum(7);
+        System.out.println();
+        downTriangleIncNum(5);
+        System.out.println();
+        upTriangle(5);
+        System.out.println();
+        rectangle(4, 5);
+        System.out.println();
+        hollow_retangle(4, 5);
+        System.out.println();
+        rohombus(6);
+        System.out.println();
+        hollow_rohombus(4, 5);
+        System.out.println();
+        // rohombus1(5);
+    }
 
-        // down triangle with increaing count
-        /*
-         * 1 2 3 4 5
-         * 6 7 8 9
-         * 10 11 12
-         * 13 14
-         * 15
-         */
-        // int n = 1;
-        // for (int i = 1; i <= 5; i++) {
-        // for (int j = 1; j <= 5 - i + 1; j++) {
-        // System.out.print(" " + n++);
-        // }
-        // System.out.println();
-        // }
-        // triangle
-        /*
-         * 12345
-         * 1234
-         * 123
-         * 12
-         * 1
-         */
-        // for (int i = 1; i <= 5; i++) {
-        // for (int j = 1; j <= 5 - i + 1; j++) {
-        // System.out.print(j);
-        // }
-        // System.out.println();
-        // }
+    public static void num_triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 3; j++) {
+    public static void downTriangleStar(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
-                if (i == j) {
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rectangle(int n, int m) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void upTriangle(int n) {
+        int k = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(k++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void square(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.err.println();
+        }
+    }
+
+    public static void downTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void downTriangleIncNum(int n) {
+        int k = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(" " + k++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void downTrianglenum(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollow_retangle(int rows, int cols) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols) {
+                    System.err.print("*");
+                } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
+        }
+    }
+
+    public static void rohombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                // spaces
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void hollow_rohombus(int rows, int cols) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                // spaces
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void rohombus1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
         }
     }
 
