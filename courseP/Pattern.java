241e5fa005fa72b91764f12378d1fba846e40384
Arpita Kashyap <arpitak1912@gmail.com>
2025-04-10 22:29:33 +0530

    Patterns questions

--- courseP/Pattern.java
+++ courseP/Pattern.java
@@ -16,29 +16,40 @@ public class Pattern {
         // System.out.println();
         // }
 
-        num_triangle(7);
-        System.out.println();
-        downTriangleStar(5);
-        System.out.println();
-        square(5);
-        System.out.println();
-        downTriangle(5);
-        System.out.println();
-        downTrianglenum(7);
-        System.out.println();
-        downTriangleIncNum(5);
-        System.out.println();
-        upTriangle(5);
-        System.out.println();
-        rectangle(4, 5);
-        System.out.println();
-        hollow_retangle(4, 5);
-        System.out.println();
-        rohombus(6);
-        System.out.println();
-        hollow_rohombus(4, 5);
-        System.out.println();
-        // rohombus1(5);
+        // num_triangle(7);
+        // System.out.println();
+        // upTriangleStar(5);
+        // System.out.println();
+        // square(5);
+        // System.out.println();
+        // downTriangle(5);
+        // System.out.println();
+        // downTrianglenum(7);
+        // System.out.println();
+        // downTriangleIncNum(5);
+        // System.out.println();
+        // upTriangle(5);
+        // System.out.println();
+        // rectangle(4, 5);
+        // System.out.println();
+        // hollow_retangle(4, 5);
+        // System.out.println();
+        // rohombus(6);
+        // System.out.println();
+        // hollow_rohombus(4, 5);
+        // System.out.println();
+        // hollowSquare(5);
+        // System.out.println();
+        // inertedDownTriangle(5);
+        // System.out.println();
+        // invertedUpTriangleStar(5);
+        // System.out.println();
+        // zeroOneTriangle(5);
+        // System.out.println();
+        // butterfly(4);
+        pyramid(4);
+        System.err.println();
+        diamond(4);
     }
 
     public static void num_triangle(int n) {
@@ -50,7 +61,7 @@ public class Pattern {
         }
     }
 
-    public static void downTriangleStar(int n) {
+    public static void upTriangleStar(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
@@ -172,4 +183,116 @@ public class Pattern {
         }
     }
 
+    public static void hollowSquare(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void inertedDownTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void invertedUpTriangleStar(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void zeroOneTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterfly(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamond(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
